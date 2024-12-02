9288028f8fa235601992401cb248b98f2424d36b
SAKSHAM GUPTA <76028123+sakshgupta@users.noreply.github.com>
2024-12-02 17:51:15 +0000

    changed UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
 import BillSplitter from "./pages/billSplitter";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <div className="w-full max-w-3xl bg-white rounded-lg shadow p-6">
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <div className="w-full max-w-3xl p-6">
         <BillSplitter />
       </div>
     </div>
